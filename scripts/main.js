a799bd65c4c4a9f2e4aaf166dd80433be0011e60
Sohaib <sohaibm131@gmail.com>
2023-01-26 20:33:59 +0000

    Modified index and main.js

--- scripts/main.js
+++ scripts/main.js
@@ -7,19 +7,20 @@
         })
 
         let mainContent = document.getElementsByTagName("main")[0]
+        mainContent.setAttribute("class", "container")
         
         // another way to access body
-        // documentBody = document.body
+        documentBody = document.body
 
         let mainParagraph = document.createElement("p")
         mainParagraph.setAttribute("id", "MainParagraph")
-        mainParagraph.setAttribute("class", "mt-3")
+        mainParagraph.setAttribute("class", "mt-3 container")
 
         // concatenation - '1' + '2' + '3'
         // interpolation - `${var_1}`
-        let firstString = "This is a main paragraph that we added through javascript"
-        // let secondString = `${ firstString } main paragraph that we added through javascript`
-        mainParagraph.textContent = firstString
+        let firstString = "This is a "
+        let secondString = `${ firstString } main paragraph that we added through javascript`
+        mainParagraph.textContent = secondString
 
         /**
          * textContent - changes text node
@@ -28,6 +29,19 @@
 
         // add after(append)
         mainContent.appendChild(mainParagraph) 
+        
+        // add before (before)
+        // mainContent.before(mainParagraph) 
+
+        // delete element
+        // document.getElementById("RandomButton").remove()
+
+        // documentBody.innerHTML = `
+        //     <div class="container">
+        //         <h1 class="display-1">Hello WEBD6201</h1>
+        //         <p class="mt-5 lead">and... what do you this of this method?</p>
+        //     </div>
+        // `
     }
     
     function Start() {
